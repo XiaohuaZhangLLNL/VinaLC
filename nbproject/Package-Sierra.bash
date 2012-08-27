3dac699bd19c5f2fefa8811275b9223d5c53ed6b
zhang30 <zhang30@scotty>
2012-08-27 15:26:21 -0700

    Fix git ignore list

--- nbproject/Package-Sierra.bash
+++ nbproject/Package-Sierra.bash
@@ -7,7 +7,7 @@
 # Macros
 TOP=`pwd`
 CND_PLATFORM=GNU-Linux-x86
-CND_CONF=Default
+CND_CONF=Sierra
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
